169b5c0de5ec73740a9b57e960e9289ddb35fcde
usausa <machi.pon@gmail.com>
2017-10-21 20:11:04 +0900

    Add metdata factory work

---
+++ ConsoleApp/Classes.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp
+{
+    public class Class0
+    {
+    }
+
+    public class Class1
+    {
+        public int Value { get; }
+
+        public Class1(int value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ ConsoleApp/Reflection/EmitTypeMetadataFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp.Reflection
+{
+    using System;
+    using System.Runtime.InteropServices.WindowsRuntime;
+
+    public class EmitTypeMetadataFactory : IActivationFactory
+    {
+        public static EmitTypeMetadataFactory Default { get; } = new EmitTypeMetadataFactory();
+
+        public object ActivateInstance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
