d2f89c4b62efb34f1ac7d8ab1379199212556b69
usausa <machi.pon@gmail.com>
2017-11-11 15:45:01 +0900

    Implement sample accessor

--- ConsoleApp/SampleAccessors.cs
+++ ConsoleApp/SampleAccessors.cs
@@ -13,9 +13,9 @@
 
         public Type Type => Source.PropertyType;
 
-        public bool CanRead { get; } = false;
+        public bool CanRead { get; } = true;
 
-        public bool CanWrite { get; } = false;
+        public bool CanWrite { get; } = true;
 
         public IntValueAccessor(PropertyInfo pi)
         {
@@ -30,6 +30,118 @@
         public void SetValue(object target, object value)
         {
             ((DataClass)target).IntValue = value == null ? default : (int)value;
+        }
+    }
+
+    public sealed class StringValueAccessor : IAccessor
+    {
+        public PropertyInfo Source { get; }
+
+        public string Name => Source.Name;
+
+        public Type Type => Source.PropertyType;
+
+        public bool CanRead { get; } = true;
+
+        public bool CanWrite { get; } = true;
+
+        public StringValueAccessor(PropertyInfo pi)
+        {
+            Source = pi;
+        }
+
+        public object GetValue(object target)
+        {
+            return ((DataClass)target).StringValue;
+        }
+
+        public void SetValue(object target, object value)
+        {
+            ((DataClass)target).StringValue = (string)value;
+        }
+    }
+
+    public sealed class IntNotificationValueAccessor : IAccessor
+    {
+        public PropertyInfo Source { get; }
+
+        public string Name => Source.Name;
+
+        public Type Type => Source.PropertyType;
+
+        public bool CanRead { get; } = true;
+
+        public bool CanWrite { get; } = true;
+
+        public IntNotificationValueAccessor(PropertyInfo pi)
+        {
+            Source = pi;
+        }
+
+        public object GetValue(object target)
+        {
+            return ((DataClass)target).IntNotificationValue.Value;
+        }
+
+        public void SetValue(object target, object value)
+        {
+            ((DataClass)target).IntNotificationValue.Value = value == null ? default : (int)value;
+        }
+    }
+
+    public sealed class StringNotificationValueAccessor : IAccessor
+    {
+        public PropertyInfo Source { get; }
+
+        public string Name => Source.Name;
+
+        public Type Type => Source.PropertyType;
+
+        public bool CanRead { get; } = true;
+
+        public bool CanWrite { get; } = true;
+
+        public StringNotificationValueAccessor(PropertyInfo pi)
+        {
+            Source = pi;
+        }
+
+        public object GetValue(object target)
+        {
+            return ((DataClass)target).StringNotificationValue.Value;
+        }
+
+        public void SetValue(object target, object value)
+        {
+            ((DataClass)target).StringNotificationValue.Value = (string)value;
+        }
+    }
+
+    public sealed class UnsuportedAccessor : IAccessor
+    {
+        public PropertyInfo Source { get; }
+
+        public string Name => Source.Name;
+
+        public Type Type => Source.PropertyType;
+
+        public bool CanRead { get; } = false;
+
+        public bool CanWrite { get; } = false;
+
+        public UnsuportedAccessor(PropertyInfo pi)
+        {
+            Source = pi;
+        }
+
+        public object GetValue(object target)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void SetValue(object target, object value)
+        {
+            throw new NotSupportedException();
         }
     }
 }
