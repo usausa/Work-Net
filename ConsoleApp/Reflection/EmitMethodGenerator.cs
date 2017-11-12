d8eb0bc18681c0567ab1fac74982116b1068496e
usausa <machi.pon@gmail.com>
2017-11-12 17:10:27 +0900

    Refactor EmitMethodGenerator

--- ConsoleApp/Reflection/EmitMethodGenerator.cs
+++ ConsoleApp/Reflection/EmitMethodGenerator.cs
@@ -1,7 +1,6 @@
 ﻿namespace ConsoleApp.Reflection
 {
     using System;
-    using System.Linq;
     using System.Reflection;
     using System.Reflection.Emit;
 
@@ -17,7 +16,53 @@
 
         private const string ModuleName = "SmartDynamicActivatorModule";
 
+        private static readonly Type ObjectType = typeof(object);
+
+        private static readonly Type VoidType = typeof(void);
+
+        private static readonly Type BoolType = typeof(bool);
+
+        private static readonly Type StringType = typeof(string);
+
         private static readonly Type CtorType = typeof(ConstructorInfo);
+
+        private static readonly Type TypeType = typeof(Type);
+
+        private static readonly Type PropertyInfoType = typeof(PropertyInfo);
+
+        private static readonly ConstructorInfo ObjectConstructor = ObjectType.GetConstructor(Type.EmptyTypes);
+
+        private static readonly MethodInfo PropertyInfoNameGetMethod =
+            PropertyInfoType.GetProperty(nameof(PropertyInfo.Name)).GetGetMethod();
+
+        private static readonly MethodInfo PropertyInfoPropertyTypeGetMethod =
+            PropertyInfoType.GetProperty(nameof(PropertyInfo.PropertyType)).GetGetMethod();
+
+        // Activator
+
+        private static readonly Type ActivatorType = typeof(IActivator);
+
+        private static readonly MethodInfo ActivatorCreateMethodInfo = ActivatorType.GetMethod(nameof(IActivator.Create));
+
+        private static readonly Type[] ActivatorConstructorArgumentTypes = { CtorType };
+
+        private static readonly Type[] ActivatorCreateArgumentTypes = { typeof(object[]) };
+
+        // Accessor
+
+        private static readonly Type AccessorType = typeof(IAccessor);
+
+        private static readonly MethodInfo AccessorGetValueMethodInfo = AccessorType.GetMethod(nameof(IAccessor.GetValue));
+
+        private static readonly MethodInfo AccessorSetValueMethodInfo = AccessorType.GetMethod(nameof(IAccessor.SetValue));
+
+        private static readonly Type[] AccessorConstructorArgumentTypes = { PropertyInfoType };
+
+        private static readonly Type[] AccessorGetValueArgumentTypes = { ObjectType };
+
+        private static readonly Type[] AccessorSetValueArgumentTypes = { ObjectType, ObjectType };
+
+        // Member
 
         private static readonly object Sync = new object();
 
@@ -45,6 +90,10 @@
             }
         }
 
+        //--------------------------------------------------------------------------------
+        // Activator
+        //--------------------------------------------------------------------------------
+
         /// <summary>
         ///
         /// </summary>
@@ -61,13 +110,30 @@
                 $"{ci.DeclaringType.FullName}_DynamicActivator",
                 TypeAttributes.Public | TypeAttributes.AutoLayout | TypeAttributes.AnsiClass | TypeAttributes.Sealed | TypeAttributes.BeforeFieldInit);
 
-            typeBuilder.AddInterfaceImplementation(typeof(IActivator));
+            typeBuilder.AddInterfaceImplementation(ActivatorType);
 
-            // Source
+            // Field
             var sourceField = typeBuilder.DefineField(
                 "source",
                 CtorType,
                 FieldAttributes.Private | FieldAttributes.InitOnly);
+
+            // Property
+            DefineActivatorPropertySource(typeBuilder, sourceField);
+
+            // Constructor
+            DefineActivatorConstructor(typeBuilder, sourceField);
+
+            // Method
+            DefineActivatorMethodCreate(typeBuilder, ci);
+
+            var typeInfo = typeBuilder.CreateTypeInfo();
+
+            return (IActivator)Activator.CreateInstance(typeInfo.AsType(), ci);
+        }
+
+        private static void DefineActivatorPropertySource(TypeBuilder typeBuilder, FieldBuilder sourceField)
+        {
             var sourceProperty = typeBuilder.DefineProperty(
                 "Source",
                 PropertyAttributes.HasDefault,
@@ -85,46 +151,228 @@
             getSourceIl.Emit(OpCodes.Ldarg_0);
             getSourceIl.Emit(OpCodes.Ldfld, sourceField);
             getSourceIl.Emit(OpCodes.Ret);
+        }
 
-            // Constructor
+        private static void DefineActivatorConstructor(TypeBuilder typeBuilder, FieldBuilder sourceField)
+        {
             var ctor = typeBuilder.DefineConstructor(
                 MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName,
                 CallingConventions.Standard,
-                new[] { CtorType });
-            var superCtor = typeof(object).GetConstructors().First();
+                ActivatorConstructorArgumentTypes);
 
             var ctorIl = ctor.GetILGenerator();
             ctorIl.Emit(OpCodes.Ldarg_0);
-            ctorIl.Emit(OpCodes.Call, superCtor);
+            ctorIl.Emit(OpCodes.Call, ObjectConstructor);
             ctorIl.Emit(OpCodes.Ldarg_0);
             ctorIl.Emit(OpCodes.Ldarg_1);
             ctorIl.Emit(OpCodes.Stfld, sourceField);
             ctorIl.Emit(OpCodes.Ret);
+        }
 
-            // Create
-            var createMethod = typeBuilder.DefineMethod(
+        private static void DefineActivatorMethodCreate(TypeBuilder typeBuilder, ConstructorInfo ci)
+        {
+            var method = typeBuilder.DefineMethod(
                 nameof(IActivator.Create),
                 MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Virtual | MethodAttributes.Final,
-                typeof(object),
-                new[] { typeof(object[]) });
-            typeBuilder.DefineMethodOverride(createMethod, typeof(IActivator).GetMethod(nameof(IActivator.Create)));
+                ObjectType,
+                ActivatorCreateArgumentTypes);
+            typeBuilder.DefineMethodOverride(method, ActivatorCreateMethodInfo);
 
-            var createIl = createMethod.GetILGenerator();
+            var ilGenerator = method.GetILGenerator();
 
             for (var i = 0; i < ci.GetParameters().Length; i++)
             {
-                createIl.Emit(OpCodes.Ldarg_1);
-                createIl.EmitLdcI4(i);
-                createIl.Emit(OpCodes.Ldelem_Ref);
-                createIl.EmitTypeConversion(ci.GetParameters()[i].ParameterType);
+                ilGenerator.Emit(OpCodes.Ldarg_1);
+                ilGenerator.EmitLdcI4(i);
+                ilGenerator.Emit(OpCodes.Ldelem_Ref);
+                ilGenerator.EmitTypeConversion(ci.GetParameters()[i].ParameterType);
             }
 
-            createIl.Emit(OpCodes.Newobj, ci);
-            createIl.Emit(OpCodes.Ret);
+            ilGenerator.Emit(OpCodes.Newobj, ci);
+            ilGenerator.Emit(OpCodes.Ret);
+        }
 
-            var typeInfo = typeBuilder.CreateTypeInfo();
+        //--------------------------------------------------------------------------------
+        // Accessor
+        //--------------------------------------------------------------------------------
 
-            return (IActivator)Activator.CreateInstance(typeInfo.AsType(), ci);
+        public static IAccessor CreateAccessor(PropertyInfo pi)
+        {
+            var typeBuilder = ModuleBuilder.DefineType(
+                $"{pi.DeclaringType.FullName}_{pi.Name}_DynamicAccsessor",
+                TypeAttributes.Public | TypeAttributes.AutoLayout | TypeAttributes.AnsiClass | TypeAttributes.Sealed | TypeAttributes.BeforeFieldInit);
+
+            typeBuilder.AddInterfaceImplementation(AccessorType);
+
+            // Fields
+            var sourceField = typeBuilder.DefineField(
+                 "source",
+                 PropertyInfoType,
+                 FieldAttributes.Private | FieldAttributes.InitOnly);
+
+            // Property
+            DefineAccessorPropertySource(typeBuilder, sourceField);
+            DefineAccessorPropertyName(typeBuilder, sourceField);
+            DefineAccessorPropertyType(typeBuilder, sourceField);
+            DefineAccessorPropertyAccessibility(typeBuilder, pi.CanRead, nameof(IAccessor.CanRead));
+            DefineAccessorPropertyAccessibility(typeBuilder, pi.CanWrite, nameof(IAccessor.CanWrite));
+
+            // Constructor
+            DefineAccessorConstructor(typeBuilder, sourceField);
+
+            // Method
+            DefineAccessorMethodGetValue(typeBuilder, pi);
+            DefineAccessorMethodSetValue(typeBuilder, pi);
+
+            var type = typeBuilder.CreateType();
+
+            return (IAccessor)Activator.CreateInstance(type, pi);
+        }
+
+        private static void DefineAccessorPropertySource(TypeBuilder typeBuilder, FieldBuilder sourceField)
+        {
+            var property = typeBuilder.DefineProperty(
+                "Source",
+                PropertyAttributes.None,
+                PropertyInfoType,
+                null);
+            var method = typeBuilder.DefineMethod(
+                "get_Source",
+                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.SpecialName | MethodAttributes.Virtual | MethodAttributes.Final,
+                PropertyInfoType,
+                Type.EmptyTypes);
+            property.SetGetMethod(method);
+
+            var ilGenerator = method.GetILGenerator();
+
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            ilGenerator.Emit(OpCodes.Ldfld, sourceField);
+            ilGenerator.Emit(OpCodes.Ret);
+        }
+
+        private static void DefineAccessorPropertyName(TypeBuilder typeBuilder, FieldBuilder sourceField)
+        {
+            var property = typeBuilder.DefineProperty(
+                "Name",
+                PropertyAttributes.None,
+                StringType,
+                null);
+            var method = typeBuilder.DefineMethod(
+                "get_Name",
+                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.SpecialName | MethodAttributes.Virtual | MethodAttributes.Final,
+                StringType,
+                Type.EmptyTypes);
+            property.SetGetMethod(method);
+
+            var ilGenerator = method.GetILGenerator();
+
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            ilGenerator.Emit(OpCodes.Ldfld, sourceField);
+            ilGenerator.Emit(OpCodes.Callvirt, PropertyInfoNameGetMethod);
+            ilGenerator.Emit(OpCodes.Ret);
+        }
+
+        private static void DefineAccessorPropertyType(TypeBuilder typeBuilder, FieldBuilder sourceField)
+        {
+            var property = typeBuilder.DefineProperty(
+                "Type",
+                PropertyAttributes.None,
+                TypeType,
+                null);
+            var method = typeBuilder.DefineMethod(
+                "get_Type",
+                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.SpecialName | MethodAttributes.Virtual | MethodAttributes.Final,
+                TypeType,
+                Type.EmptyTypes);
+            property.SetGetMethod(method);
+
+            var ilGenerator = method.GetILGenerator();
+
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            ilGenerator.Emit(OpCodes.Ldfld, sourceField);
+            ilGenerator.Emit(OpCodes.Callvirt, PropertyInfoPropertyTypeGetMethod);
+            ilGenerator.Emit(OpCodes.Ret);
+        }
+
+        private static void DefineAccessorPropertyAccessibility(TypeBuilder typeBuilder, bool enable, string name)
+        {
+            var property = typeBuilder.DefineProperty(
+                name,
+                PropertyAttributes.None,
+                BoolType,
+                null);
+            var method = typeBuilder.DefineMethod(
+                $"get_{name}",
+                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.SpecialName | MethodAttributes.Virtual | MethodAttributes.Final,
+                BoolType,
+                Type.EmptyTypes);
+            property.SetGetMethod(method);
+
+            var ilGenerator = method.GetILGenerator();
+
+            if (enable)
+            {
+                ilGenerator.Emit(OpCodes.Ldc_I4_1);
+            }
+            else
+            {
+                ilGenerator.Emit(OpCodes.Ldc_I4_1);
+            }
+            ilGenerator.Emit(OpCodes.Ret);
+        }
+
+        private static void DefineAccessorConstructor(TypeBuilder typeBuilder, FieldBuilder sourceField)
+        {
+            var ctor = typeBuilder.DefineConstructor(
+                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.SpecialName |
+                MethodAttributes.RTSpecialName,
+                CallingConventions.Standard,
+                AccessorConstructorArgumentTypes);
+
+            var ilGenerator = ctor.GetILGenerator();
+
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            ilGenerator.Emit(OpCodes.Call, ObjectConstructor);
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            ilGenerator.Emit(OpCodes.Ldarg_1);
+            ilGenerator.Emit(OpCodes.Stfld, sourceField);
+            ilGenerator.Emit(OpCodes.Ret);
+        }
+
+        private static void DefineAccessorMethodGetValue(TypeBuilder typeBuilder, PropertyInfo pi)
+        {
+            var method = typeBuilder.DefineMethod(
+                nameof(IActivator.Create),
+                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Virtual | MethodAttributes.Final,
+                ObjectType,
+                AccessorGetValueArgumentTypes);
+            typeBuilder.DefineMethodOverride(method, AccessorGetValueMethodInfo);
+
+            var ilGenerator = method.GetILGenerator();
+
+            ilGenerator.Emit(OpCodes.Ldarg_1);
+            ilGenerator.Emit(OpCodes.Castclass, pi.DeclaringType);
+            ilGenerator.Emit(OpCodes.Callvirt, pi.GetGetMethod());
+            ilGenerator.Emit(OpCodes.Ret);
+        }
+
+        private static void DefineAccessorMethodSetValue(TypeBuilder typeBuilder, PropertyInfo pi)
+        {
+            var method = typeBuilder.DefineMethod(
+                nameof(IActivator.Create),
+                MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Virtual | MethodAttributes.Final,
+                VoidType,
+                AccessorSetValueArgumentTypes);
+            typeBuilder.DefineMethodOverride(method, AccessorSetValueMethodInfo);
+
+            var ilGenerator = method.GetILGenerator();
+
+            ilGenerator.Emit(OpCodes.Ldarg_1);
+            ilGenerator.Emit(OpCodes.Castclass, pi.DeclaringType);
+            ilGenerator.Emit(OpCodes.Ldarg_2);
+            ilGenerator.Emit(OpCodes.Castclass, pi.PropertyType);
+            ilGenerator.Emit(OpCodes.Callvirt, pi.GetSetMethod());
+            ilGenerator.Emit(OpCodes.Ret);
         }
     }
 }
