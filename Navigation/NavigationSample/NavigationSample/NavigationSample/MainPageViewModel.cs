c924faf64f51e809102ef9a94fb0bc601e20094f
usausa <machi.pon@gmail.com>
2020-09-17 19:55:10 +0900

    Update navigation

---
+++ Navigation/NavigationSample/NavigationSample/NavigationSample/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace NavigationSample
+{
+    using NavigationSample.Shell;
+
+    using Smart.ComponentModel;
+    using Smart.Forms.ViewModels;
+    using Smart.Navigation;
+
+    public class MainPageViewModel : ViewModelBase, IShellControl
+    {
+        public static MainPageViewModel DesignInstance { get; } = null; // For design
+
+        public NotificationValue<string> Title { get; } = new NotificationValue<string>();
+
+        public ApplicationState ApplicationState { get; }
+
+        public INavigator Navigator { get; }
+
+        //--------------------------------------------------------------------------------
+        // Constructor
+        //--------------------------------------------------------------------------------
+
+        public MainPageViewModel(
+            ApplicationState applicationState,
+            INavigator navigator)
+            : base(applicationState)
+        {
+            ApplicationState = applicationState;
+            Navigator = navigator;
+        }
+    }
+}
---
+++ Navigation/NavigationSample/NavigationSample/NavigationSample/Models/Entity/DataEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace NavigationSample.Models.Entity
+{
+    public class DataEntity
+    {
+        public static DataEntity DesignInstance { get; } = null; // For design
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
