c9ecf415798d1269b4e6591dfa44736931fd0230
usausa <machi.pon@gmail.com>
2020-11-27 09:56:19 +0900

    Update work

--- WorkMisc/WorkMisc/Program.cs
+++ WorkMisc/WorkMisc/Program.cs
@@ -1,9 +1,44 @@
 ﻿namespace WorkMisc
 {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Columns;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Running;
+
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            BenchmarkRunner.Run<Benchmark>();
         }
+    }
+
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            AddExporter(MarkdownExporter.Default, MarkdownExporter.GitHub);
+            AddColumn(
+                StatisticColumn.Mean,
+                StatisticColumn.Min,
+                StatisticColumn.Max,
+                StatisticColumn.P90,
+                StatisticColumn.Error,
+                StatisticColumn.StdDev);
+            AddDiagnoser(MemoryDiagnoser.Default);
+            AddJob(Job.MediumRun);
+        }
+    }
+
+    [Config(typeof(BenchmarkConfig))]
+    public class Benchmark
+    {
     }
 }
