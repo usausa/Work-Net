4081a8c97caa607255e10b23274bfa8ad796ce6f
usausa <machi.pon@gmail.com>
2021-09-17 19:10:12 +0900

    Add work project

---
+++ WorkCall/WorkCall/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace WorkCall
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+
+        private static void TestInterface()
+        {
+            IAction action = new SealedAction();
+            action.Invoke();
+        }
+
+        private static void TestNonSealed()
+        {
+            var action = new NonSealedAction();
+            action.Invoke();
+        }
+
+        private static void TestSealed()
+        {
+            var action = new SealedAction();
+            action.Invoke();
+        }
+
+        private static void TestAction()
+        {
+            Action action = () => { };
+            action.Invoke();
+        }
+
+        private static void TestArgumentSealed(SealedAction action)
+        {
+            action.Invoke();
+        }
+
+        private static void TestArgumentAction(Action action)
+        {
+            action.Invoke();
+        }
+    }
+
+    public interface IAction
+    {
+        void Invoke();
+    }
+
+    public class NonSealedAction : IAction
+    {
+        public void Invoke()
+        {
+        }
+    }
+
+    public sealed class SealedAction : IAction
+    {
+        public void Invoke()
+        {
+        }
+    }
+}
