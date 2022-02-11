1dc0e2180dd9bc859a57bfa3ab77da878514bc41
usausa <machi.pon@gmail.com>
2022-02-11 12:48:33 +0900

    Update

---
+++ WorkBuffer/CheckBenchmark/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Runtime.CompilerServices;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace CheckBenchmark;
+
+public static class Program
+{
+    public static void Main()
+    {
+        BenchmarkRunner.Run<Benchmark>();
+    }
+}
+
+public class BenchmarkConfig : ManualConfig
+{
+    public BenchmarkConfig()
+    {
+        AddExporter(MarkdownExporter.Default, MarkdownExporter.GitHub);
+        AddColumn(
+            StatisticColumn.Mean,
+            StatisticColumn.Min,
+            StatisticColumn.Max,
+            StatisticColumn.P90,
+            StatisticColumn.Error,
+            StatisticColumn.StdDev);
+        AddDiagnoser(MemoryDiagnoser.Default);
+        AddJob(Job.MediumRun);
+    }
+}
+
+[Config(typeof(BenchmarkConfig))]
+public class Benchmark
+{
+    // 0.21  0
+    // 0.21  0
+    // 0.43 24
+    // 0.49 24
+
+    private const int N = 1000;
+
+    [Benchmark(OperationsPerInvoke = N)]
+    public void ClassDispose()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            using var _ = new ClassDisposable();
+        }
+    }
+
+    [Benchmark(OperationsPerInvoke = N)]
+    public void StructDispose()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            using var _ = new StructDisposable();
+        }
+    }
+
+
+    [Benchmark(OperationsPerInvoke = N)]
+    public void ClassArgument()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Call(new ClassDisposable());
+        }
+    }
+
+    [Benchmark(OperationsPerInvoke = N)]
+    public void StructArgument()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Call(new StructDisposable());
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static void Call(IDisposable disposable)
+    {
+        using var _ = disposable;
+    }
+}
+
+public sealed class ClassDisposable : IDisposable
+{
+    public void Dispose()
+    {
+    }
+}
+
+public struct StructDisposable : IDisposable
+{
+    public void Dispose()
+    {
+    }
+}
---
+++ WorkBuffer/WorkBuffer/PooledByteBuffer.cs
@@ -0,0 +1,93 @@
+﻿namespace WorkBuffer
+{
+    using System.Buffers;
+    using System.Buffers.Text;
+    using System.Runtime.CompilerServices;
+
+    public sealed class PooledByteBuffer
+    {
+        private byte[] buffer;
+
+        private int length;
+
+        public bool IsEmpty
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => length == 0;
+        }
+
+        public PooledByteBuffer(int bufferSize)
+        {
+            buffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+            length = 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose()
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan() => new(buffer, 0, length);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Memory<byte> AsMemory() => new(buffer, 0, length);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Clear() => length = 0;
+
+        private void Grow(int size)
+        {
+            var newSize = buffer.Length;
+            do
+            {
+                newSize *= 2;
+            }
+            while (newSize < length + size);
+
+            var newBuffer = ArrayPool<byte>.Shared.Rent(newSize);
+            buffer.CopyTo(newBuffer, 0);
+            ArrayPool<byte>.Shared.Return(buffer);
+            buffer = newBuffer;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(ReadOnlySpan<byte> value)
+        {
+            if ((buffer.Length - length) < value.Length)
+            {
+                Grow(value.Length);
+            }
+
+            value.CopyTo(buffer.AsSpan(length));
+            length += value.Length;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(byte value)
+        {
+            if ((buffer.Length - length) < 1)
+            {
+                Grow(1);
+            }
+
+            buffer[length++] = value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(int value)
+        {
+            if (!Utf8Formatter.TryFormat(value, buffer.AsSpan(length), out var written))
+            {
+                Grow(written);
+                if (!Utf8Formatter.TryFormat(value, buffer.AsSpan(length), out written))
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+
+            length += written;
+        }
+    }
+}
