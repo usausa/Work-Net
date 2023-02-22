171e02f5e633edd45ca4ae77ba64d034b5b13064
usausa <machi.pon@gmail.com>
2023-02-22 17:21:58 +0900

    Update

--- Work/WorkTextInflector/Program.cs
+++ Work/WorkTextInflector/Program.cs
@@ -43,25 +43,28 @@ public class Benchmark
 {
     private const int N = 1000;
 
-    //[Benchmark]
-    //public void Pascalize()
-    //{
-    //    for (var i = 0; i < N; i++)
-    //    {
-    //        Inflector.Pascalize("abc_xyz");
-    //    }
-    //}
-
-    //[Benchmark]
-    //public void Camelize()
-    //{
-    //    for (var i = 0; i < N; i++)
-    //    {
-    //        Inflector.Camelize("abc_xyz");
-    //    }
-    //}
+    [Benchmark]
+    [BenchmarkCategory("Pascalize")]
+    public void Pascalize()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Inflector.Pascalize("abc_xyz");
+        }
+    }
 
     [Benchmark]
+    [BenchmarkCategory("Camelize")]
+    public void Camelize()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Inflector.Camelize("abc_xyz");
+        }
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Underscore")]
     public void Underscore()
     {
         for (var i = 0; i < N; i++)
@@ -71,6 +74,7 @@ public class Benchmark
     }
 
     [Benchmark]
+    [BenchmarkCategory("UnderscoreUpper")]
     public void UnderscoreUpper()
     {
         for (var i = 0; i < N; i++)
@@ -81,9 +85,28 @@ public class Benchmark
 
     // 2
 
-    // TODO
+    [Benchmark]
+    [BenchmarkCategory("Pascalize")]
+    public void Pascalize2()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Inflector2.Pascalize("abc_xyz");
+        }
+    }
 
     [Benchmark]
+    [BenchmarkCategory("Camelize")]
+    public void Camelize2()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Inflector2.Camelize("abc_xyz");
+        }
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Underscore")]
     public void Underscore2()
     {
         for (var i = 0; i < N; i++)
@@ -93,11 +116,54 @@ public class Benchmark
     }
 
     [Benchmark]
+    [BenchmarkCategory("UnderscoreUpper")]
     public void UnderscoreUpper2()
     {
         for (var i = 0; i < N; i++)
         {
             Inflector2.Underscore("AbcXyz", true);
+        }
+    }
+
+    // 3
+
+    [Benchmark]
+    [BenchmarkCategory("Pascalize")]
+    public void Pascalize3()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Inflector3.Pascalize("abc_xyz");
+        }
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Camelize")]
+    public void Camelize3()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Inflector3.Camelize("abc_xyz");
+        }
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("Underscore")]
+    public void Underscore3()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Inflector3.Underscore("AbcXyz");
+        }
+    }
+
+    [Benchmark]
+    [BenchmarkCategory("UnderscoreUpper")]
+    public void UnderscoreUpper3()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            Inflector3.Underscore("AbcXyz", true);
         }
     }
 }
@@ -229,47 +295,46 @@ public static class Inflector2
         }
 
         var buffer = word.Length < 512 ? stackalloc char[word.Length] : new char[word.Length];
-        var length = 0;
 
-        // TODO
-        fixed (char* pBuffer = buffer)
+        ref var start = ref MemoryMarshal.GetReference(buffer);
+        ref var b = ref start;
+
+        var isLowerPrevious = false;
+        foreach (var c in word)
         {
-            var isLowerPrevious = false;
-            foreach (var c in word)
+            if (c == '_')
             {
-                if (c == '_')
+                toUpper = true;
+            }
+            else
+            {
+                if (toUpper)
                 {
-                    toUpper = true;
+                    b = Char.ToUpperInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
+                    toUpper = false;
+                }
+                else if (isLowerPrevious)
+                {
+                    b = c;
+                    b = ref Unsafe.Add(ref b, 1);
                 }
                 else
                 {
-                    if (toUpper)
-                    {
-                        pBuffer[length++] = Char.ToUpperInvariant(c);
-                        toUpper = false;
-                    }
-                    else if (isLowerPrevious)
-                    {
-                        pBuffer[length++] = c;
-                    }
-                    else
-                    {
-                        pBuffer[length++] = Char.ToLowerInvariant(c);
-                    }
-
-                    isLowerPrevious = Char.IsLower(c);
+                    b = Char.ToLowerInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
                 }
-            }
 
-            return new string(pBuffer, 0, length);
+                isLowerPrevious = Char.IsLower(c);
+            }
         }
+
+        return new string(buffer[..((int)Unsafe.ByteOffset(ref start, ref b) >> 1)]);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string Underscore(ReadOnlySpan<char> word) => Underscore(word, false);
 
-    // TODO 最初のcを特殊化することでUnsafe.IsAddressGreaterThanを減らせるか? v3
-    // TODO word, cもUnsafeベース?、EndとUnsafe.IsAddressLessThan?
     public static unsafe string Underscore(ReadOnlySpan<char> word, bool toUpper)
     {
         if (word.IsEmpty)
@@ -325,6 +390,156 @@ public static class Inflector2
                     b = c;
                     b = ref Unsafe.Add(ref b, 1);
                 }
+            }
+        }
+
+        return new string(buffer[..((int)Unsafe.ByteOffset(ref start, ref b) >> 1)]);
+    }
+}
+
+public static class Inflector3
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string Pascalize(ReadOnlySpan<char> word) => Camelize(word, true);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string Camelize(ReadOnlySpan<char> word) => Camelize(word, false);
+
+    public static unsafe string Camelize(ReadOnlySpan<char> word, bool toUpper)
+    {
+        if (word.IsEmpty)
+        {
+            return string.Empty;
+        }
+
+        var length = word.Length;
+        var buffer = length < 512 ? stackalloc char[length] : new char[length];
+
+        ref var start = ref MemoryMarshal.GetReference(buffer);
+        ref var b = ref start;
+
+        ref var c = ref MemoryMarshal.GetReference(word);
+        ref var end = ref Unsafe.Add(ref c, length);
+
+        var isLowerPrevious = false;
+        do
+        {
+            if (c == '_')
+            {
+                toUpper = true;
+            }
+            else
+            {
+                if (toUpper)
+                {
+                    b = Char.ToUpperInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
+                    toUpper = false;
+                }
+                else if (isLowerPrevious)
+                {
+                    b = c;
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+                else
+                {
+                    b = Char.ToLowerInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+
+                isLowerPrevious = Char.IsLower(c);
+            }
+
+            c = ref Unsafe.Add(ref c, 1);
+        } while (Unsafe.IsAddressLessThan(ref c, ref end));
+
+        return new string(buffer[..((int)Unsafe.ByteOffset(ref start, ref b) >> 1)]);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string Underscore(ReadOnlySpan<char> word) => Underscore(word, false);
+
+    public static unsafe string Underscore(ReadOnlySpan<char> word, bool toUpper)
+    {
+        if (word.IsEmpty)
+        {
+            return string.Empty;
+        }
+
+        var length = word.Length;
+        var bufferSize = length << 1;
+        var buffer = bufferSize < 512 ? stackalloc char[bufferSize] : new char[bufferSize];
+
+        ref var start = ref MemoryMarshal.GetReference(buffer);
+        ref var b = ref start;
+
+        ref var c = ref MemoryMarshal.GetReference(word);
+        ref var end = ref Unsafe.Add(ref c, length);
+
+        if (toUpper)
+        {
+            if (Char.IsUpper(c))
+            {
+                b = c;
+                b = ref Unsafe.Add(ref b, 1);
+            }
+            else
+            {
+                b = Char.ToUpperInvariant(c);
+                b = ref Unsafe.Add(ref b, 1);
+            }
+
+            c = ref Unsafe.Add(ref c, 1);
+
+            while (Unsafe.IsAddressLessThan(ref c, ref end))
+            {
+                if (Char.IsUpper(c))
+                {
+                    b = '_';
+                    b = ref Unsafe.Add(ref b, 1);
+                    b = c;
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+                else
+                {
+                    b = Char.ToUpperInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+
+                c = ref Unsafe.Add(ref c, 1);
+            }
+        }
+        else
+        {
+            if (Char.IsUpper(c))
+            {
+                b = Char.ToLowerInvariant(c);
+                b = ref Unsafe.Add(ref b, 1);
+            }
+            else
+            {
+                b = c;
+                b = ref Unsafe.Add(ref b, 1);
+            }
+
+            c = ref Unsafe.Add(ref c, 1);
+
+            while (Unsafe.IsAddressLessThan(ref c, ref end))
+            {
+                if (Char.IsUpper(c))
+                {
+                    b = '_';
+                    b = ref Unsafe.Add(ref b, 1);
+                    b = Char.ToLowerInvariant(c);
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+                else
+                {
+                    b = c;
+                    b = ref Unsafe.Add(ref b, 1);
+                }
+
+                c = ref Unsafe.Add(ref c, 1);
             }
         }
 
