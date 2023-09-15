7f78df953917cb7330125c4e4cfaf189046a56da
usausa <machi.pon@gmail.com>
2023-09-15 22:33:44 +0900

    Add project

---
+++ Work/WorkStaticInterface/Program.cs
@@ -0,0 +1,30 @@
+﻿Test.Display<Data1>();
+Test.Display<Data2>();
+
+public static class Test
+{
+    // IL_0000:  constrained. !!T
+    // IL_0006:  call int32 IMetadata::Size()
+    // IL_000b:  call void [System.Console] System.Console::WriteLine(int32)
+    // IL_0010:  ret
+    public static void Display<T>()
+        where T : IMetadata
+    {
+        Console.WriteLine(T.Size());
+    }
+}
+
+public interface IMetadata
+{
+    static abstract int Size();
+}
+
+public class Data1 : IMetadata
+{
+    public static int Size() => 4;
+}
+
+public class Data2 : IMetadata
+{
+    public static int Size() => 8;
+}
