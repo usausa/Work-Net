e2eba7b8493f0758178e96e6107984c042ff40b9
usausa <machi.pon@gmail.com>
2023-10-08 18:18:18 +0900

    Update

---
+++ WorkWeb/WorkStartup/Controllers/ApplicationStartupFilter.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkStartup.Controllers;
+
+using System.Diagnostics;
+
+public class ApplicationStartupFilter : IStartupFilter
+{
+    public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+    {
+        return builder =>
+        {
+            //builder.ApplicationServices.GetService()
+
+            Debug.WriteLine("* Startup");
+
+            next(builder);
+        };
+    }
+}
