c34961b7ebc97882028190b2365637c6d87eb1db
usausa <machi.pon@gmail.com>
2023-10-09 09:32:28 +0900

    Update

--- WorkWeb/WorkCompress/Program.cs
+++ WorkWeb/WorkCompress/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
+using System.Net.Mime;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Compress
+builder.Services.AddRequestDecompression();
+builder.Services.AddResponseCompression(options =>
+{
+    // Default false (for CRIME and BREACH attacks)
+    options.EnableForHttps = true;
+    options.Providers.Add<GzipCompressionProvider>();
+    options.MimeTypes = new[] { MediaTypeNames.Application.Json };
+});
+builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.Fastest;
+});
 
 var app = builder.Build();
 
@@ -19,6 +37,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Compress
+app.UseResponseCompression();
+app.UseRequestDecompression();
 
 app.MapControllers();
 
