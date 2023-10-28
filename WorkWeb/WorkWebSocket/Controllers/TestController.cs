d211c13c4364a71cf0df89edfa5bd59ba0776ea4
usausa <machi.pon@gmail.com>
2023-10-28 16:43:27 +0900

    Update

---
+++ WorkWeb/WorkWebSocket/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+namespace WorkWebSocket.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Execute()
+    {
+        return Ok();
+    }
+}
