3955cd8127b408f28e12e70b3abe28d1747ec4a1
usausa <machi.pon@gmail.com>
2024-02-24 21:36:07 +0900

    Add project baseline

---
+++ WorkWeb/WorkExceptionMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WorkExceptionMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
