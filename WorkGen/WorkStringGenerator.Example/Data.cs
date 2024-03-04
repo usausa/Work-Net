662318ca144b4f92e92f7581265b21506a9031ba
usausa <machi.pon@gmail.com>
2024-03-04 11:48:32 +0900

    Update

--- WorkGen/WorkStringGenerator.Example/Data.cs
+++ WorkGen/WorkStringGenerator.Example/Data.cs
@@ -1,9 +1,13 @@
 namespace WorkStringGenerator.Example;
 
+#pragma warning disable CA1819
 [ToString]
 public partial class Data
 {
     public int Id { get; set; }
 
     public string Name { get; set; } = default!;
+
+    public int[] Values { get; set; } = default!;
 }
+#pragma warning restore CA1819
--- WorkGen/WorkStringGenerator.Example/Program.cs
+++ WorkGen/WorkStringGenerator.Example/Program.cs
@@ -1,12 +1,13 @@
 namespace WorkStringGenerator.Example;
 
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 public static class Program
 {
     public static void Main()
     {
-        var d = new Data { Id = 123, Name = "Data-xyz" };
-        Debug.WriteLine(d.ToString());
+        Debug.WriteLine(new Data { Id = 123, Name = "Data-xyz", Values = [1, 2] });
+        Debug.WriteLine(new Data { Id = 123, Name = "Data-xyz" });
     }
 }
