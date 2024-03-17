9caa3b7f3043b3ae525a839e75290c0920ee250f
usausa <machi.pon@gmail.com>
2024-03-17 21:30:39 +0900

    Update

---
+++ Work/WorkGitDraw/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkGitDraw;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
