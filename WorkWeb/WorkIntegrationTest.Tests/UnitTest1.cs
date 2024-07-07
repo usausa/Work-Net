778d3bb5ff58a4ceb3c01bb4700b9a4533097ac1
usausa <machi.pon@gmail.com>
2024-07-07 18:35:41 +0900

    Update test

--- WorkWeb/WorkIntegrationTest.Tests/UnitTest1.cs
+++ WorkWeb/WorkIntegrationTest.Tests/UnitTest1.cs
@@ -1,10 +1,41 @@
 namespace WorkIntegrationTest.Tests;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
 
+using Newtonsoft.Json;
+using System.Text;
+using WorkIntegrationTest.Web.Controllers;
+
+public class UnitTest1 : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> factory;
+    private readonly HttpClient client;
+
+    public UnitTest1(WebApplicationFactory<Program> factory)
+    {
+        this.factory = factory;
+        client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Test1()
+    {
+        // Arrange
+        var requestObject = new Request
+        {
+            Value = "Test"
+        };
+        var jsonContent = new StringContent(JsonConvert.SerializeObject(requestObject), Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await client.PostAsync("/test/execute", jsonContent);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        var responseObject = JsonConvert.DeserializeObject<Response>(await response.Content.ReadAsStringAsync());
+
+        Assert.Equal("Test", responseObject.Value);
     }
 }
--- WorkWeb/WorkIntegrationTest.Web/Program.cs
+++ WorkWeb/WorkIntegrationTest.Web/Program.cs
@@ -21,3 +21,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program
+{
+}
