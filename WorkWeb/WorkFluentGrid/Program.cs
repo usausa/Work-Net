28709082f9ddb02cc55a719da887f8bccb05c216
usausa <machi.pon@gmail.com>
2024-08-05 14:27:40 +0900

    Add project

---
+++ WorkWeb/WorkFluentGrid/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+
+using WorkFluentGrid.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
