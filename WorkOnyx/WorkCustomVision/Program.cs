0c20899b3786eea852d9aeab3a19c297dfe1b1b2
usausa <machi.pon@gmail.com>
2024-08-09 15:19:29 +0900

    Update

--- WorkOnyx/WorkCustomVision/Program.cs
+++ WorkOnyx/WorkCustomVision/Program.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using System.Diagnostics;
 
 using Microsoft.ML.OnnxRuntime;
@@ -9,10 +10,15 @@ using var session = new InferenceSession("model.onnx");
 var metadata = session.InputMetadata.First();
 var dimensions = metadata.Value.Dimensions;
 
-var inputTensor = LoadImageFile("image.jpg", dimensions);
+var size = 3 * dimensions[2] * dimensions[3];
+var buffer = ArrayPool<float>.Shared.Rent(size);
+var inputTensor = new DenseTensor<float>(buffer.AsMemory(0, size), [1, 3, dimensions[2], dimensions[3]]);
+LoadImageFile("image.jpg", inputTensor, dimensions[2], dimensions[3]);
 
 var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor(metadata.Key, inputTensor) };
 using var results = session.Run(inputs);
+
+ArrayPool<float>.Shared.Return(buffer);
 
 var labels = File.ReadAllLines("labels.txt");
 
@@ -46,23 +52,20 @@ for (var i = 0; i < Math.Min(scores.Length, 10); i++)
 using var outputStream = File.OpenWrite("output.jpg");
 skBitmap.Encode(outputStream, SKEncodedImageFormat.Jpeg, 100);
 
-static DenseTensor<float> LoadImageFile(string imagePath, int[] dimensions)
+static void LoadImageFile(string imagePath, DenseTensor<float> tensor, int width, int height)
 {
     using var inputStream = File.OpenRead(imagePath);
     using var skBitmap = SKBitmap.Decode(inputStream);
-    var resizedBitmap = skBitmap.Resize(new SKImageInfo(dimensions[2], dimensions[3]), SKFilterQuality.High);
+    var resizedBitmap = skBitmap.Resize(new SKImageInfo(width, height), SKFilterQuality.High);
 
-    var input = new DenseTensor<float>([1, 3, dimensions[2], dimensions[3]]);
     for (var y = 0; y < resizedBitmap.Height; y++)
     {
         for (var x = 0; x < resizedBitmap.Width; x++)
         {
             var color = resizedBitmap.GetPixel(x, y);
-            input[0, 0, y, x] = color.Red - 255.0f;
-            input[0, 1, y, x] = color.Green - 255.0f;
-            input[0, 2, y, x] = color.Blue - 255.0f;
+            tensor[0, 0, y, x] = color.Red - 255.0f;
+            tensor[0, 1, y, x] = color.Green - 255.0f;
+            tensor[0, 2, y, x] = color.Blue - 255.0f;
         }
     }
-
-    return input;
 }
