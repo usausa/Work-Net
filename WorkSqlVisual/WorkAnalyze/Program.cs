af8083698c19968d02792d0a961b680c3b52556d
usausa <machi.pon@gmail.com>
2024-08-29 19:25:36 +0900

    Update

--- WorkSqlVisual/WorkAnalyze/Program.cs
+++ WorkSqlVisual/WorkAnalyze/Program.cs
@@ -1,8 +1,31 @@
-﻿namespace WorkAnalyze;
+namespace WorkAnalyze;
 
-internal class Program
+using Microsoft.Data.SqlClient;
+
+using System.Diagnostics;
+
+public static class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
+        using var con = new SqlConnection("Data Source=db-server;Initial Catalog=Test;User ID=test;Password=test;TrustServerCertificate=True;");
+        con.Open();
+
+        using var cmd = con.CreateCommand();
+        cmd.CommandText = "SET STATISTICS XML ON; SELECT * FROM Data WHERE Id = 1; SET STATISTICS XML OFF;";
+
+        var reader = cmd.ExecuteReader();
+        do
+        {
+            while (reader.Read())
+            {
+                if (reader.FieldCount > 0)
+                {
+                    Debug.WriteLine(reader.GetName(0));
+                    Debug.WriteLine(reader.GetString(0));
+                }
+            }
+        }
+        while (reader.NextResult());
     }
 }
