1d7820b4fad5b30a585290e2dd8e6d0e19a157ba
usausa <machi.pon@gmail.com>
2024-09-22 14:47:06 +0900

    Update

--- Work/WorkBleCrossBasic/Program.cs
+++ Work/WorkBleCrossBasic/Program.cs
@@ -1,5 +1,6 @@
 namespace WorkBleCrossBasic;
 
+using System;
 using System.Diagnostics;
 
 using InTheHand.Bluetooth;
@@ -10,12 +11,22 @@ internal class Program
     {
         Bluetooth.AdvertisementReceived += (_, @event) =>
         {
-            if (String.IsNullOrEmpty(@event.Name))
-            {
-                return;
-            }
+            //if (String.IsNullOrEmpty(@event.Name))
+            //{
+            //    return;
+            //}
 
-            Debug.WriteLine($"{@event.Uuids.Length} {@event.Name} {@event.Rssi} {@event.TxPower}");
+            //Debug.WriteLine($"{@event.Uuids.Length} {@event.Name} {@event.Rssi} {@event.TxPower}");
+
+            if (@event.ManufacturerData.TryGetValue(0x0969, out var buffer))
+            {
+                if (buffer.Length >= 11)
+                {
+                    var temperature = ((float)(buffer[8] & 0x0f) / 10 + (buffer[9] & 0x7f)) * ((buffer[9] & 0x80) > 0 ? 1 : -1);
+                    var humidity = buffer[10] & 0x7f;
+                    Debug.WriteLine($"{DateTime.Now:HH:mm:ss.fff} {@event.Rssi} {Convert.ToHexString(buffer.AsSpan(0, 6))} Temp={temperature:F1}C, Hum={humidity}%");
+                }
+            }
         };
         var scan = await Bluetooth.RequestLEScanAsync();
 
