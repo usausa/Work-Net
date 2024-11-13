bdb4233a785bfec4869c0ce52ffc2ce96dd2ae56
usausa <machi.pon@gmail.com>
2024-11-13 11:43:52 +0900

    Update

--- Work/WorkClientInflux/Program.cs
+++ Work/WorkClientInflux/Program.cs
@@ -1,8 +1,44 @@
-namespace WorkClientInflux;
+using System.Diagnostics;
+using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Core;
 
-internal static class Program
+var url = args[0];
+var organization = args[1];
+var bucket = args[2];
+var token = args[3];
+
+using var client = new InfluxDBClient(url, token);
+
+var rand = new Random();
+while (true)
 {
-    internal static void Main()
+    var writer = client.GetWriteApiAsync();
+
+    var value = new Speed
     {
+        DateTime = DateTime.UtcNow,
+        Download = rand.NextDouble() * 1000,
+        Upload = rand.NextDouble() * 1000
+    };
+    try
+    {
+        await writer.WriteMeasurementAsync(value, WritePrecision.Ns, bucket, organization);
     }
+    catch (Exception e)
+    {
+        Debug.WriteLine(e);
+    }
+
+    await Task.Delay(10_000);
+}
+
+
+
+[Measurement("Speed")]
+public class Speed
+{
+    [Column(IsTimestamp = true)] public DateTime DateTime { get; set; }
+    [Column("Download")] public double Download { get; set; }
+    [Column("Upload")] public double Upload { get; set; }
 }
