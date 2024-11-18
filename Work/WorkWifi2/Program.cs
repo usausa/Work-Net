198616ae0f8a44f62582025086e9ee41eaf36414
usausa <machi.pon@gmail.com>
2024-11-18 16:18:35 +0900

    Update

--- Work/WorkWifi2/Program.cs
+++ Work/WorkWifi2/Program.cs
@@ -1,11 +1,14 @@
+using System.Diagnostics;
 using ManagedNativeWifi;
 
 while (true)
 {
+    var watch = Stopwatch.StartNew();
     foreach (var net in NativeWifi.EnumerateBssNetworks())
     {
-        Console.WriteLine($"{net.Ssid} {net.BssType} {net.Band} {net.Channel} {net.SignalStrength}");
+        Console.WriteLine($"{net.Bssid} {net.Ssid} {net.BssType} {net.Band} {net.Channel} {net.SignalStrength}");
     }
+    Console.WriteLine($"--{watch.ElapsedMilliseconds}");
 
     await Task.Delay(1000);
 }
