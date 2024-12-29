cf1dbe3dd46eee1606d8861088e71762639a65ea
usausa <machi.pon@gmail.com>
2024-12-29 09:05:50 +0900

    Update

---
+++ Work/WorkDeviceVu1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkDeviceVu1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- WorkAspireManual/WorkAspire.AppHost/Program.cs
+++
@@ -1,5 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.WorkAspire_ApiService>("workaspire-apiservice");
-
-builder.Build().Run();
