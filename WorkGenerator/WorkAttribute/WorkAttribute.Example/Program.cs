3a15a99f54a00ef5cad3979ff456a18723a20fab
usausa <machi.pon@gmail.com>
2025-01-18 18:07:56 +0900

    Update

---
+++ WorkGenerator/WorkAttribute/WorkAttribute.Example/Program.cs
@@ -0,0 +1,8 @@
+namespace WorkAttribute.Example;
+
+internal static class Program
+{
+    public static void Main()
+    {
+    }
+}
---
+++ WorkGenerator/WorkAttribute/WorkAttribute/Generator.cs
@@ -0,0 +1,12 @@
+namespace WorkAttribute;
+
+using Microsoft.CodeAnalysis;
+
+[Generator]
+public sealed class Generator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // TODO
+    }
+}
