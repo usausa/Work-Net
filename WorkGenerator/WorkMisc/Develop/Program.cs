8c92cb95faf4bb8a1dc6b30e350f4c91404bb487
usausa <machi.pon@gmail.com>
2025-01-20 10:13:37 +0900

    Update library

---
+++ WorkGenerator/WorkMisc/Develop/Program.cs
@@ -0,0 +1,8 @@
+namespace Develop;
+
+internal static class Program
+{
+    public static void Main()
+    {
+    }
+}
---
+++ WorkGenerator/WorkMisc/WorkMisc.Generator/MiscGenerator.cs
@@ -0,0 +1,14 @@
+namespace WorkMisc.Generator;
+
+using System.Text;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+[Generator]
+public sealed class MiscGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+    }
+}
