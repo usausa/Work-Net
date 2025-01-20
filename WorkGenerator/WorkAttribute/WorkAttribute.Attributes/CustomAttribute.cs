d3c8fb21196ddc49fd2812e55a9f6b4e20e2e0da
usausa <machi.pon@gmail.com>
2025-01-20 09:23:34 +0900

    Update

---
+++ WorkGenerator/WorkAttribute/WorkAttribute.Attributes/CustomAttribute.cs
@@ -0,0 +1,8 @@
+namespace WorkAttribute.Attributes;
+
+using System;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class CustomAttribute : Attribute
+{
+}
--- WorkGenerator/WorkAttribute/WorkAttribute/Generator.cs
+++ WorkGenerator/WorkAttribute/WorkAttribute/Generator.cs
@@ -1,12 +1,18 @@
 namespace WorkAttribute;
 
+using System.Text;
+
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
 
 [Generator]
 public sealed class Generator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        // TODO
+        context.RegisterPostInitializationOutput(ctx =>
+        {
+            ctx.AddSource("Work", SourceText.From("// dummy", Encoding.UTF8));
+        });
     }
 }
