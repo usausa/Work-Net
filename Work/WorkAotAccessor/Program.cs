0aa6ca16c229b4dea56885e045ea4d0523deb276
usausa <machi.pon@gmail.com>
2025-01-28 14:14:15 +0900

    Update

--- Work/WorkAotAccessor/Program.cs
+++ Work/WorkAotAccessor/Program.cs
@@ -6,14 +6,30 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var accessorFactory = AccessorRegistry.FindFactory<Data>()!;
-        var getter = accessorFactory.CreateGetter<int>(nameof(Data.Id))!;
-        var setter = accessorFactory.CreateSetter<int>(nameof(Data.Id))!;
+        // Required
+        // rd.xml
 
-        var data = new Data();
-        setter(data, 123);
-        var id = getter(data);
+        // TODO
+        // Generic
+        // NoWarn IL
+
+        var accessorFactory1 = AccessorRegistry.FindFactory<Data>()!;
+        var getter1 = accessorFactory1.CreateGetter<int>(nameof(Data.Id))!;
+        var setter1 = accessorFactory1.CreateSetter<int>(nameof(Data.Id))!;
+
+        var data1 = new Data();
+        setter1(data1, 123);
+        var id = getter1(data1);
         Console.WriteLine(id);
+
+        //var accessorFactory2 = AccessorRegistry.FindFactory<GenericData<int>>()!;
+        //var getter2 = accessorFactory2.CreateGetter<int>(nameof(GenericData<int>.Value))!;
+        //var setter2 = accessorFactory2.CreateSetter<int>(nameof(GenericData<int>.Value))!;
+
+        //var data2 = new GenericData<int>();
+        //setter2(data2, 123);
+        //var value = getter2(data2);
+        //Console.WriteLine(value);
     }
 }
 
@@ -23,4 +39,10 @@ public partial class Data
     public int Id { get; set; }
 
     public string Name { get; set; } = default!;
+}
+
+[GenerateAccessor]
+public class GenericData<T>
+{
+    public T Value { get; set; } = default!;
 }
