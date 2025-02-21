70a4b1d3ee066169574e03f4914480b8d3fae18d
usausa <machi.pon@gmail.com>
2025-02-21 13:48:06 +0900

    Update

--- LibMvvm/WorkHost/Program.cs
+++ LibMvvm/WorkHost/Program.cs
@@ -1,15 +1,21 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using WorkHost;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<WpfHostedService<App>>();
-
-builder.Services.AddSingleton<App>();
-builder.Services.AddSingleton<MainWindow>();
 
 Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
 Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
 
+// TODO Folder
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// TODO Log, Config, ...
+
+builder.Services.AddSingleton<App>();
+builder.Services.AddSingleton<MainWindow>();
+
 var host = builder.Build();
-host.Run();
+
+var app = host.Services.GetRequiredService<App>();
+app.Run();
---
+++ LibMvvm/WorkHostMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkHostMaui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
