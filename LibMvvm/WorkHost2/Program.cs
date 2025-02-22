0ff8efbde2735fccf28bb0822f4afb44d0339047
usausa <machi.pon@gmail.com>
2025-02-22 16:05:37 +0900

    Update

--- LibMvvm/WorkHost2/Program.cs
+++ LibMvvm/WorkHost2/Program.cs
@@ -3,13 +3,13 @@ using Microsoft.Extensions.Hosting;
 using WorkHost2;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<WpfHostedService<App>>();
 
 builder.Services.AddSingleton<App>();
+builder.Services.AddHostedService<WpfHostedService<App>>();
 builder.Services.AddSingleton<MainWindow>();
 
-Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
-Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
+//Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
+//Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
 
 var host = builder.Build();
 host.Run();
