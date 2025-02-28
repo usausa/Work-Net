8cd691269e1ae92b0ab3201710593be4aeec19d9
usausa <machi.pon@gmail.com>
2025-02-28 09:10:52 +0900

    Update

--- WorkWeb/WorkApiAuth/WorkApiAuth/Controllers/TestController.cs
+++ WorkWeb/WorkApiAuth/WorkApiAuth/Controllers/TestController.cs
@@ -36,18 +36,16 @@ public class TestController : ControllerBase
             new(ClaimTypes.Name, request.User),
             new(ClaimTypes.Role, "Administrator")
         };
-        var identify = new ClaimsIdentity(claims, "custom");
 
         var tokenHandler = new JwtSecurityTokenHandler();
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = identify,
-            Expires = DateTime.UtcNow.AddDays(AuthenticationSettings.ExpireDays),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(AuthenticationSettings.SecretKey)), SecurityAlgorithms.HmacSha256Signature),
-            Audience = AuthenticationSettings.Audience,
-            Issuer = AuthenticationSettings.Issuer
-        };
-        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var token = new JwtSecurityToken(
+            AuthenticationSettings.Issuer,
+            AuthenticationSettings.Audience,
+            claims,
+            expires: DateTime.UtcNow.AddDays(AuthenticationSettings.ExpireDays),
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(AuthenticationSettings.SecretKey)), SecurityAlgorithms.HmacSha256Signature)
+        );
+
         return Ok(new LoginResponse
         {
             Token = tokenHandler.WriteToken(token)
