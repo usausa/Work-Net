c0cc7f35adf40ad2828a5053e3fe9c84d1a2d836
usausa <machi.pon@gmail.com>
2025-05-24 22:44:35 +0900

    Update

---
+++ Work/WorkStringLang/Program.cs
@@ -0,0 +1,18 @@
+namespace WorkStringLang;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // lang=json
+        var json = """
+                   {
+                   	 "name": "Usa Usa",
+                   	 "age": 17,
+                   	 "enabled": true
+                   }
+                   """;
+        // lang=xml
+        var xml = "<data>123</data>";
+    }
+}
