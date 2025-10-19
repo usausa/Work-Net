94e1c7c4e69055a160e34f1fe23328361d938e6d
usausa <machi.pon@gmail.com>
2025-10-19 18:21:12 +0900

    Update

---
+++ WorkService/WorkTcpClient/Program.cs
@@ -0,0 +1,21 @@
+namespace WorkTcpClient;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        // TODO
+    }
+}
+
+
+
+public sealed class CommandClientFactory
+{
+
+}
+
+public sealed class CommandClient
+{
+
+}
