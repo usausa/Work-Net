cdaf98c20162b774353c6fa75ead6b3a15892be9
usausa <machi.pon@gmail.com>
2025-11-03 16:52:29 +0900

    Update

--- WorkGrpc/ChatClient/Program.cs
+++ WorkGrpc/ChatClient/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ChatServer;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+Console.Write("Enter name: ");
+var name = Console.ReadLine() ?? "Anonymous";
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new ChatApi.ChatApiClient(channel);
+
+using var chat = client.SendMessage();
+
+// Receiver
+var readerTask = Task.Run(async () =>
+{
+    try
+    {
+        // ReSharper disable once AccessToDisposedClosure
+        await foreach (var message in chat.ResponseStream.ReadAllAsync())
+        {
+            var timestamp = message.Timestamp.ToDateTime().ToLocalTime();
+            Console.WriteLine($"[{timestamp:HH:mm:ss}] {message.Name}: {message.Message}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"受信エラー: {ex.Message}");
+    }
+});
+
+// Sender
+try
+{
+    while (true)
+    {
+        var input = Console.ReadLine();
+
+        if (String.IsNullOrEmpty(input))
+        {
+            continue;
+        }
+
+        if (input.ToLower() == "exit")
+        {
+            break;
+        }
+
+        await chat.RequestStream.WriteAsync(new ChatMessage { Name = name, Message = input });
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Unknown exception: {ex.Message}");
+}
+finally
+{
+    await chat.RequestStream.CompleteAsync();
+}
+
+await readerTask;
--- WorkGrpc/ChatServer/Api/ChatApi.cs
+++ WorkGrpc/ChatServer/Api/ChatApi.cs
@@ -1,21 +1,84 @@
 namespace ChatServer.Api;
 using ChatServer;
 
+using Google.Protobuf.WellKnownTypes;
+
 using Grpc.Core;
 
 using System.Collections.Concurrent;
 
 public class ChatApi : ChatServer.ChatApi.ChatApiBase
 {
-    // TODO 分離
-    private static readonly ConcurrentBag<IServerStreamWriter<ChatMessage>> clients = new();
+    // TODO Divide
+    private static readonly ConcurrentDictionary<string, IServerStreamWriter<ChatMessage>> Subscribers = new();
+    private static int connectionCounter;
 
-    public override Task SendMessage(IAsyncStreamReader<ChatMessage> requestStream, IServerStreamWriter<ChatMessage> responseStream, ServerCallContext context)
+    private readonly ILogger<ChatApi> log;
+
+    public ChatApi(ILogger<ChatApi> log)
     {
-        clients.Add(responseStream);
+        this.log = log;
+    }
 
+    public override async Task SendMessage(IAsyncStreamReader<ChatMessage> requestStream, IServerStreamWriter<ChatMessage> responseStream, ServerCallContext context)
+    {
+        var connectionId = $"{Interlocked.Increment(ref connectionCounter)}#{context.Peer}";
+        Subscribers.TryAdd(connectionId, responseStream);
 
-        // TODO
-        return Task.CompletedTask;
+        try
+        {
+            // TODO add Timeout token
+            await foreach (var message in requestStream.ReadAllAsync(context.CancellationToken))
+            {
+                var timestamp = DateTime.UtcNow;
+
+                log.LogInformation("Message received. timestamp=[{timestamp:HH:mm:ss}], connectionId=[{connectionId}], name=[{name}], message=[{message}]", timestamp, connectionId, message.Name, message.Message);
+
+                message.Timestamp = Timestamp.FromDateTime(timestamp);
+                await BroadcastMessageAsync(message);
+            }
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex, "Unknown exception. id=[{connectionId}]", connectionId);
+        }
+        finally
+        {
+            if (Subscribers.TryRemove(connectionId, out _))
+            {
+                log.LogInformation("Subscriber disconnected. connectionId=[{connectionId}]", connectionId);
+            }
+        }
+    }
+
+    private async Task BroadcastMessageAsync(ChatMessage message)
+    {
+        var deadSubscribers = default(List<string>);
+
+        foreach (var (connectionId, writer) in Subscribers)
+        {
+            try
+            {
+                await writer.WriteAsync(message);
+            }
+            catch (Exception ex)
+            {
+                log.LogWarning(ex, "Send message failed. connectionId=[{connectionId}]", connectionId);
+
+                deadSubscribers ??= new List<string>();
+                deadSubscribers.Add(connectionId);
+            }
+        }
+
+        if (deadSubscribers is not null)
+        {
+            foreach (var connectionId in deadSubscribers)
+            {
+                if (Subscribers.TryRemove(connectionId, out _))
+                {
+                    log.LogInformation("Subscriber removed. connectionId=[{connectionId}]", connectionId);
+                }
+            }
+        }
     }
 }
