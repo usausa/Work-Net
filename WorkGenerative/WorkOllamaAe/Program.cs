00bd04a2c4fa56305fd529efdda94d0b7b5880e1
usausa <machi.pon@gmail.com>
2025-11-29 19:15:15 +0900

    Add project

---
+++ WorkGenerative/WorkOllamaAe/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.AI;
+
+using OllamaSharp;
+
+var chatClient = new OllamaApiClient(new Uri("http://ollama:11434/"), "gpt-oss:20b");
+
+while (true)
+{
+    Console.Write("Prompt: ");
+    var prompt = Console.ReadLine();
+
+    if (String.IsNullOrWhiteSpace(prompt))
+    {
+        break;
+    }
+
+    await foreach (var update in chatClient.GetStreamingResponseAsync(prompt))
+    {
+        Console.Write(update.Text);
+    }
+
+    Console.WriteLine();
+}
