69f7decf3e56b30d435410c83354c3cc78ef6e47
usausa <machi.pon@gmail.com>
2026-01-11 19:44:39 +0900

    Add

---
+++ WorkHmi/WorkHmi/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+
+namespace WorkHmi
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+#if !DEBUG
+        Position = new Avalonia.PixelPoint(0, 0);
+        WindowState = WindowState.FullScreen;
+#endif
+        }
+    }
+}
