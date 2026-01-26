bc1ea2d37537f0cfde330014237d5c72eb79384a
usausa <machi.pon@gmail.com>
2026-01-26 14:43:06 +0900

    Update

---
+++ WorkPi/WorkMailbox/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkMailbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
