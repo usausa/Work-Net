df5c3671abfbd03f6c8c7f5f4d421d81f0c4e209
usausa <machi.pon@gmail.com>
2026-01-26 14:43:11 +0900

    Update

---
+++ WorkHid/WorkTssUsb/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkTssUsb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
