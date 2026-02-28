c6843a5dc209aa414727a33382fad3f65fb67e34
usausa <machi.pon@gmail.com>
2026-02-28 21:39:34 +0900

    Update

---
+++ WorkHmiCodex53/WorkHmiCodex53/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WorkHmiCodex53
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
