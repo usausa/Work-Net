ccdb774c394665dbca4a6babf0457ad28d09a162
usausa <machi.pon@gmail.com>
2026-03-25 09:13:54 +0900

    Update

--- WorkSlack/WorkSlack/Models/Channel.cs
+++
@@ -1,8 +0,0 @@
-namespace WorkSlack.Models;
-
-public record Channel(
-    string Id,
-    string Name,
-    string? Description = null,
-    bool IsDirectMessage = false,
-    string? DmUserId = null);
