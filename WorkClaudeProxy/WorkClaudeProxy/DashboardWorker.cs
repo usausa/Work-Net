576c22c5a5369fcb48c2a972333ce3a998ea9267
usausa <machi.pon@gmail.com>
2026-04-15 08:43:49 +0900

    Update

--- WorkClaudeProxy/WorkClaudeProxy/DashboardWorker.cs
+++ WorkClaudeProxy/WorkClaudeProxy/DashboardWorker.cs
@@ -1,21 +1,67 @@
+using HidSharp;
+using LcdDriver.TrofeoVision;
+
 namespace WorkClaudeProxy;
 
 internal sealed class DashboardWorker : BackgroundService
 {
-    private readonly DashboardImageStore imageStore;
+    private const int RetryDelaySeconds = 5;
 
-    public DashboardWorker(DashboardImageStore imageStore)
+    private readonly DashboardImageStore imageStore;
+    private readonly ILogger<DashboardWorker> logger;
+
+    public DashboardWorker(DashboardImageStore imageStore, ILogger<DashboardWorker> logger)
     {
         this.imageStore = imageStore;
+        this.logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+#pragma warning disable CA1031
+            try
+            {
+                await RunDisplayLoopAsync(stoppingToken).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
+            {
+                break;
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning(ex, "LCD display error.");
+            }
+#pragma warning restore CA1031
+
+            if (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(RetryDelaySeconds), stoppingToken).ConfigureAwait(false);
+            }
+        }
+    }
+
+    private async Task RunDisplayLoopAsync(CancellationToken stoppingToken)
+    {
+        var hidDevice = DeviceList.Local
+            .GetHidDevices(ScreenDevice.VendorId, ScreenDevice.ProductId)
+            .FirstOrDefault();
+        if (hidDevice is null)
+        {
+            return;
+        }
+
+        using var screen = new ScreenDevice(hidDevice);
+
         using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
         while (await timer.WaitForNextTickAsync(stoppingToken).ConfigureAwait(false))
         {
             var image = imageStore.GetImage();
-            // TODO: send image to LCD display
+            if (image is not null)
+            {
+                screen.DrawJpeg(image);
+            }
         }
     }
 }
