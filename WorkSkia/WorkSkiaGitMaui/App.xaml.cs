78509e78ffdf77219b742e9335fadead15bec730
usausa <machi.pon@gmail.com>
2026-05-13 09:17:44 +0900

    Update

---
+++ WorkSkia/WorkSkiaGitMaui/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace WorkSkiaGitMaui;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ WorkSkia/WorkSkiaGitMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkSkiaGitMaui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
