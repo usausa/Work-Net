75b9a63b34a37cf7febfeec04389a6e4693c23b1
usausa <machi.pon@gmail.com>
2026-05-15 09:49:50 +0900

    Update

---
+++ WorkSkia/WorkGauge/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace WorkGauge;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ WorkSkia/WorkGauge/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkGauge;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
